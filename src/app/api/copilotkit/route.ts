d3bcc0d3ee6fafc01dd356e6d440ee17bb1ee905
Ayush Dhamecha <ayuahjiteshdhamecha@gmail.com>
2024-08-08 00:01:29 +0530

    last commit

--- src/app/api/copilotkit/route.ts
+++ src/app/api/copilotkit/route.ts
@@ -6,7 +6,7 @@ import {
   copilotRuntimeNextJSAppRouterEndpoint,
   OpenAIAdapter,
 } from "@copilotkit/runtime";
-import OpenAI from "openai/index.mjs";
+import OpenAI from "openai";
 
 const UNSPLASH_ACCESS_KEY_ENV = "UNSPLASH_ACCESS_KEY";
 const UNSPLASH_ACCESS_KEY = process.env[UNSPLASH_ACCESS_KEY_ENV];
