9613f9bb5d0283d6c6542c2a05d411a2bb6f0c87
Ayush Dhamecha <ayuahjiteshdhamecha@gmail.com>
2024-08-07 15:30:46 +0530

    first commit

--- src/app/api/copilotkit/route.ts
+++ src/app/api/copilotkit/route.ts
@@ -6,14 +6,14 @@ import {
   copilotRuntimeNextJSAppRouterEndpoint,
   OpenAIAdapter,
 } from "@copilotkit/runtime";
+import OpenAI from "openai";
 
 const UNSPLASH_ACCESS_KEY_ENV = "UNSPLASH_ACCESS_KEY";
 const UNSPLASH_ACCESS_KEY = process.env[UNSPLASH_ACCESS_KEY_ENV];
 
 const researchAction: Action<any> = {
   name: "research",
-  description:
-    "Call this function to conduct research on a certain topic. Respect other notes about when to call this function",
+  description: "Call this function to conduct research on a certain topic. Respect other notes about when to call this function",
   parameters: [
     {
       name: "topic",
@@ -72,13 +72,28 @@ export const POST = async (req: NextRequest) => {
     actions.push(researchAction);
   }
 
-  const openaiModel = process.env["OPENAI_MODEL"];
+  const AZURE_OPENAI_API_KEY = process.env["AZURE_OPENAI_API_KEY"];
+  if (!AZURE_OPENAI_API_KEY) {
+    throw new Error("The AZURE_OPENAI_API_KEY environment variable is missing or empty.");
+  }
 
-  console.log("ENV.COPILOT_CLOUD_API_KEY", process.env.COPILOT_CLOUD_API_KEY);
+  // The name of your Azure OpenAI Instance and model deployment
+  const instance = "viresh";
+  const model = "gpt-4o";
+
+  const openai = new OpenAI({
+    apiKey: AZURE_OPENAI_API_KEY,
+    baseURL: `https://${instance}.openai.azure.com/openai/deployments/${model}`,
+    defaultQuery: { "api-version": "2023-03-15-preview" },
+    defaultHeaders: { "api-key": AZURE_OPENAI_API_KEY },
+  });
+
+  const runtime = new CopilotRuntime({ actions });
+  const serviceAdapter = new OpenAIAdapter({ openai });
 
   const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
-    runtime: new CopilotRuntime({ actions }),
-    serviceAdapter: new OpenAIAdapter({ model: openaiModel }),
+    runtime,
+    serviceAdapter,
     endpoint: req.nextUrl.pathname,
   });
 
